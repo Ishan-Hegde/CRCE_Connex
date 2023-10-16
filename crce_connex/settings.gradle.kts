6efae66daea3c512ab137088f83ffdb6d8a88078
IshanFelix <hegdeishan10@gmail.com>
2023-10-16 15:05:19 +0530

    asbasbabsas

---
+++ crce_connex/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "crce_connex"
+include(":app")
+include(":myapplication")
